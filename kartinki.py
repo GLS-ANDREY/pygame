e4c5f21bab53b2f0e497dcda28de29511c0091d7
zhuko <zhukov_ai@outlook.com>
2023-05-23 14:33:00 +0300

    там парам

--- kartinki.py
+++ kartinki.py
@@ -2,12 +2,12 @@ import pygame
 import random
 import time
 
+t = pygame.Surface([1000, 1000])
 
-t = pygame.Surface([1000,1000])
+br = pygame.Rect(0, 0, 30, 30)
+pygame.draw.rect(t, [230, 40, 155], br)
 
-
-niger = pygame.Rect(0,0,30,30)
-pygame.draw.rect(t,[0,0,20],niger)
-
-pygame.image.save(t, "niger.jpg")
-
+g = pygame.image.load("aboba.png")
+pygame.image.save(t, "br.jpg")
+t.blit(g, [200, 100])
+pygame.image.save(t, "br.jpg")
