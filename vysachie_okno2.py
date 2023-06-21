e3169d788c5246acb954b1e3414b22bd4886b49b
zhuko <zhukov_ai@outlook.com>
2023-06-21 15:15:53 +0300

    там парам

--- vysachie_okno2.py
+++ vysachie_okno2.py
@@ -5,18 +5,18 @@ import pygame
 t = pygame.display.set_mode([1000, 700])
 
 s = ["Это его папа", "ulibka.png", "Это его брат", "popugai.png"]
-s2 = random.choice(s)
-s3 = os.path.exists(s2)
 
-if s3 == True:
-    u = pygame.image.load(s2)
-    t.blit(u, [0, 0])
+for a in s:
+    s3 = os.path.exists(a)
+    if s3 == False:
+        pygame.init()
+        b = pygame.font.SysFont("arial", 80, True, True)
+        bb = b.render(a, True, [200, 200, 200], [0, 255, 0])
+        t.blit(bb, [random.randint(0, 600), random.randint(0, 600)])
 
-if s3 == False:
-    pygame.init()
-    b = pygame.font.SysFont("arial", 80, True, True)
-    bb = b.render(s2, True, [200, 200, 200], [0, 255, 0])
-    t.blit(bb, [0, 0])
+    if s3 == True:
+        u = pygame.image.load(a)
+        t.blit(u, [random.randint(0, 600), random.randint(0, 600)])
 
 while True:
     pygame.display.flip()
