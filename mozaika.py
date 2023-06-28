d3cdc729ffd6117c771482b36124b3b44095dbae
zhuko <zhukov_ai@outlook.com>
2023-06-28 16:08:11 +0300

    там парам

--- mozaika.py
+++ mozaika.py
@@ -5,10 +5,18 @@ import pygame
 t = pygame.display.set_mode([1000, 700])
 
 p = pygame.image.load("popugai.png")
+u = pygame.image.load("ulibka.png")
+st = pygame.image.load("stick.png")
+s = [u,p,st]
+
 b = 0
-for a in range(5):
-    t.blit(p, [0, b])
-    b = b + 50
+for a in range(10):
+    mr = random.choice(s)
+    tr = pygame.transform.scale(mr,[random.randint(20,120),random.randint(20,120)])
+    sv = tr.get_height()
+    t.blit(tr, [0, b])
+    b = b + sv
+
 
 while True:
     pygame.display.flip()
