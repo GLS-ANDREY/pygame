dcdf380f97a329cd916eaaefa21d3a270109c5ab
zhuko <zhukov_ai@outlook.com>
2023-09-14 19:29:00 +0300

    там парам

--- new_sheet.py
+++ new_sheet.py
@@ -4,13 +4,18 @@ import pygame
 t = pygame.display.set_mode([1000, 700])
 
 p = pygame.image.load("popugai.png")
+u = pygame.image.load("ulibka.png")
 
-tr = pygame.transform.scale(p,[random.randint(20,200),random.randint(20,200)])
-gw = tr.get_width()
+tr = pygame.transform.scale(p,[random.randint(150,300),random.randint(150,300)])
 gh = tr.get_height()
+gw = tr.get_width()
+tu = pygame.transform.scale(u,[gw/2,gh/2])
 t.blit(tr,[100,100])
-t.blit(tr,[gh,gw])
-
+t.blit(tr,[gw+100,100])
+t.blit(tr,[100,gh+100])
+t.blit(tu,[100+gw,100+gh])
+pygame.draw.rect(t,[200, 200, 200],[gw+100,gh+100,gw,gh],5)
+pygame.draw.rect(t,[201, 201, 201],[100,100,gw+gw,gh+gh],5)
 
 
 while True:
